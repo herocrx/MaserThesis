2b5088a4df936867ca3055b94be48078ddf7ef06
Hubert Kuc <heroadm@IdeaPad-Z510>
2017-05-02 00:33:30 +0200

    Hubert HUD changewq

--- Tuto8.cpp
+++ Tuto8.cpp
@@ -193,7 +193,8 @@ typedef std::vector< osg::ref_ptr<osg::Image> > ImageList;
 
 	    // create a model from the images and pass it to the viewer.
 	    viewer->setSceneData(createModel());
-
+        // I think we can do something here
+        // Let's do it tomorrow
 
 	    return viewer->run();
 	}
